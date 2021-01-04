e32191d6f4437948e3aee3be0066d0916477d4fe
vortydev <etienne.dmenard@gmail.com>
2021-01-04 15:52:24 -0500

    Main menu and credits created

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1); // Loads scene 1 with the actual game
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit(); // Quits the application
+        Debug.Log("Quit");
+    }
+}
