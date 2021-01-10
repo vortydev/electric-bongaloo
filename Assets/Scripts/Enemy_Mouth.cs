92d5d272c6372a47cbe28f83f618db0974676da4
C10ak3d <c10ak3d.01@gmail.com>
2021-01-10 05:34:11 -0700

    Built BigMouth again, Prefabed it.
    Prefabed him into each of the rooms as well. He definately increases the dificulty.

--- Assets/Scripts/Enemy_Mouth.cs
+++ Assets/Scripts/Enemy_Mouth.cs
@@ -2,45 +2,84 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy_Mouth : Enemy
+public class Enemy_Mouth : Roaming
 {
-    Animator mouth;
+    Roaming roaming;
+    Animator animator;
+    [SerializeField] GameObject coneHandler;
+
 
     void Start()
     {
-        mouth = gameObject.GetComponent<Animator>();
+        roaming = gameObject.GetComponent<Roaming>();
+        animator = gameObject.GetComponent<Animator>();
+        roaming.SetDestination(gameObject);
     }
 
     void AnimRight()
     {
-        mouth.SetBool("face_Right", true);
-        mouth.SetBool("face_Left", !true);
-        mouth.SetBool("face_Back", !true);
-        mouth.SetBool("face_Front", !true);
+        animator.SetBool("face_Right", true);
+        animator.SetBool("face_Left", !true);
+        animator.SetBool("face_Back", !true);
+        animator.SetBool("face_Front", !true);
     }
 
     void AnimLeft()
     {
-        mouth.SetBool("face_Right", !true);
-        mouth.SetBool("face_Left", true);
-        mouth.SetBool("face_Back", !true);
-        mouth.SetBool("face_Front", !true);
+        animator.SetBool("face_Right", !true);
+        animator.SetBool("face_Left", true);
+        animator.SetBool("face_Back", !true);
+        animator.SetBool("face_Front", !true);
     }
 
     void AnimFront()
     {
-        mouth.SetBool("face_Right", !true);
-        mouth.SetBool("face_Left", !true);
-        mouth.SetBool("face_Back", !true);
-        mouth.SetBool("face_Front", true);
+        animator.SetBool("face_Right", !true);
+        animator.SetBool("face_Left", !true);
+        animator.SetBool("face_Back", !true);
+        animator.SetBool("face_Front", true);
     }
 
     void AnimBack()
     {
-        mouth.SetBool("face_Right", !true);
-        mouth.SetBool("face_Left", !true);
-        mouth.SetBool("face_Back", true);
-        mouth.SetBool("face_Front", !true);
+        animator.SetBool("face_Right", !true);
+        animator.SetBool("face_Left", !true);
+        animator.SetBool("face_Back", true);
+        animator.SetBool("face_Front", !true);
     }
+    void ChangeAnimDirection()
+    {//x- faceRight, x+ faceLeft, y- faceBack, y+ faceFront
+        float dirX = gameObject.transform.position.x - roaming.GetDestination().transform.position.x;
+        float dirY = gameObject.transform.position.y - roaming.GetDestination().transform.position.y;
 
+        if (Mathf.Abs(dirX) > Mathf.Abs(dirY))
+        {
+            if (dirX > 0)
+            {
+                AnimLeft();
+            }
+            else AnimRight();
+        }
+        else
+        {
+            if (dirY > 0)
+            {
+                AnimFront();
+            }
+            else AnimBack();
+        }
+    }
+    void Update()
+    {
+        if (!GetIsWaiting())
+        {
+            coneHandler.SetActive(true);
+            if (roaming.GetDestination() != gameObject)
+            {
+                ChangeAnimDirection();
+                MoveToWaypoint(GetVector2());
+            }
+        }
+        else coneHandler.SetActive(false);
+    }
 }
---
+++ Assets/Scripts/Enemy_Mouth_Cone.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Mouth_Cone : MonoBehaviour
+{
+    Roaming roaming;
+
+    [SerializeField] float rotationSpeed = 0.3f;
+    GameObject go;
+
+    void Start()
+    {
+        go = gameObject;
+        roaming = gameObject.transform.parent.gameObject.GetComponent<Roaming>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(collision.gameObject);
+        if (collision.name == ("Player"))
+        {            
+            roaming.SetDestination(collision.gameObject);
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!roaming.GetIsWaiting())
+        go.transform.Rotate(0,0,rotationSpeed);
+    }
+}
